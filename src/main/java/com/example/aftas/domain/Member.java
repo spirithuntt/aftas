5b1b6411c8c89197b26edeb8e973775df8e9e152
spirithuntt <nouhaila.elhousnii@gmail.com>
2023-12-12 01:20:01 +0100

    created the member request dto

--- src/main/java/com/example/aftas/domain/Member.java
+++ src/main/java/com/example/aftas/domain/Member.java
@@ -20,6 +20,7 @@ public class Member {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private Integer number;
 
     private String name;
---
+++ src/main/java/com/example/aftas/dto/MemberRequestDTO.java
@@ -0,0 +1,43 @@
+package com.example.aftas.dto;
+
+import com.example.aftas.domain.Member;
+import com.example.aftas.domain.enums.IdentityDocumentType;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDate;
+
+public record MemberRequestDTO(
+
+    @NotNull(message = "Name can't be null")
+    @NotNull(message = "Name can't be blank")
+    @Size(max = 50, min = 2, message = "Name must be between 2 and 50 characters")
+    String name,
+
+    @NotNull(message = "Family name can't be null")
+    @NotNull(message = "Family name can't be blank")
+    @Size(max = 50, min = 2, message = "Family name must be between 2 and 50 characters")
+    String familyName,
+
+    @NotNull(message = "Nationality can't be null")
+    @NotNull(message = "Nationality can't be blank")
+    @Size(max = 50, min = 2, message = "Nationality must be between 2 and 50 characters")
+    String nationality,
+
+    IdentityDocumentType identityDocumentType,
+
+    @NotNull(message = "Identity number can't be null")
+    @NotNull(message = "Identity number can't be blank")
+    @Size(max = 50, min = 2, message = "Identity number must be between 2 and 50 characters")
+    String identityNumber
+) {
+    public Member toMember() {
+        return Member.builder()
+                .name(name)
+                .familyName(familyName)
+                .nationality(nationality)
+                .identityDocumentType(identityDocumentType)
+                .identityNumber(identityNumber)
+                .build();
+    }
+}
