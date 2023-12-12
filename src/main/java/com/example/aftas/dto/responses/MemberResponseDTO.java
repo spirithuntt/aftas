f50916ba54f6f456054bc9b9441861a4b8f45f4e
spirithuntt <nouhaila.elhousnii@gmail.com>
2023-12-12 09:51:02 +0100

    created the member response dto

---
+++ src/main/java/com/example/aftas/dto/responses/MemberResponseDTO.java
@@ -0,0 +1,28 @@
+package com.example.aftas.dto.responses;
+
+import com.example.aftas.domain.Member;
+import com.example.aftas.domain.enums.IdentityDocumentType;
+
+import java.time.LocalDate;
+
+public record MemberResponseDTO(
+    Integer number,
+    String name,
+    String familyName,
+    LocalDate accessionDate,
+    String nationality,
+    IdentityDocumentType identityDocumentType,
+    String identityNumber
+) {
+    public static MemberResponseDTO fromMember(Member member){
+        return new MemberResponseDTO(
+                member.getNumber(),
+                member.getName(),
+                member.getFamilyName(),
+                member.getAccessionDate(),
+                member.getNationality(),
+                member.getIdentityDocumentType(),
+                member.getIdentityNumber()
+        );
+    }
+}
